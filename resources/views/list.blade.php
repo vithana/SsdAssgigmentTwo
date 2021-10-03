b25158c63d583085690e815c50a5e74ae7d4226e
Nipuna Manoratne <nip.manoratne@gmail.com>
2021-10-03 21:06:07 +0530

    submit button change

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,23 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <h1 class="align-self-center">
-            Upload Your Files To Your Google Drive
-        </h1>
-        <form action="/drive/upload" method="post" enctype="multipart/form-data" class="mb-4 mt-4">
-            <input type="file" name="file">
-            <input type="submit" value="Submit" >
-            {{csrf_field()}}
-        </form>
-        @for ($i = count($files); $i >= 1; $i--)
-            <div class="btn-outline-primary mt-2">
-                {{$files[$i-1]}}
-            </div>
-
-        @endfor
+<div class="container">
+    <h1 class="align-self-center">
+        Upload Your Files To Your Google Drive
+    </h1>
+    <form action="/drive/upload" method="post" enctype="multipart/form-data" class="mb-4 mt-4">
+        <input type="file" name="file">
+        <button class="btn btn-success" type="submit">Submit</button>
+        {{csrf_field()}}
+    </form>
+    @for ($i = count($files); $i >= 1; $i--)
+    <div class="btn-outline-primary mt-2">
+        {{$files[$i-1]}}
     </div>
+
+    @endfor
+</div>
 @endsection
-
-
-
-
